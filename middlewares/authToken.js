09017fb4cedbe0aaa455be9f8fc24aa297458f58
Nelson Andrés Mora López <na2003ml.am@gmail.com>
2023-10-07 17:47:28 -0500

    Mejor interpretación de errores Se mejoró la interpretación del token en el middleware

--- middlewares/authToken.js
+++ middlewares/authToken.js
@@ -6,16 +6,21 @@ class Authenticator {
 
     verifyToken = async (req, res, next) => {
         try {
-            const bearerHeader = req.headers['Authorization'];
+            const bearerHeader = req.headers['authorization'];
             if (typeof bearerHeader !== 'undefined') {
                 const bearerToken = bearerHeader.split(' ')[1];
                 const decode = jwt.decode(bearerToken, process.env.PRIVATE_KEY);
-                const empleado = EmpleadoService.read(decode.email);
-                if (empleado.length === 0) {
-                    res.status(404).json({ message: "Empleado not found" });
+                if (decode == null) {
+                    res.status(403).json({ message: "Token error" });
                 } else {
-                    next();
+                    const empleado = EmpleadoService.read(decode.email);
+                    if (empleado.length === 0) {
+                        res.status(404).json({ message: "Empleado not found" });
+                    } else {
+                        next();
+                    }
                 }
+
             } else {
                 res.status(403).json({ message: "No token provided" });
             }
